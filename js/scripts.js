9f69b496350b23a5b4d8e4dfc475cb37ab2721d3
Mihocsa Szilard <mihocsa48@gmail.com>
2021-05-31 20:08:15 +0200

    beautifying

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,24 @@
 //creating the Pokemon database
-
-let pokemonList = [{name: 'Pikachu', height: 0.4, type:'electric'},
-                   {name: 'Charizard', height: 1.7, type:['fire', 'flying']},
-                   {name: 'Onix', height: 8.8, type:['rock', 'ground']},
-                   {name: 'Cubone', height: 0.4, type:'ground'}];
+let pokemonList = [{
+    name: 'Pikachu',
+    height: 0.4,
+    type: 'electric'
+  },
+  {
+    name: 'Charizard',
+    height: 1.7,
+    type: ['fire', 'flying']
+  },
+  {
+    name: 'Onix',
+    height: 8.8,
+    type: ['rock', 'ground']
+  },
+  {
+    name: 'Cubone',
+    height: 0.4,
+    type: 'ground'
+  }
+];
 
 console.log(pokemonList);
