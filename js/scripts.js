4dd7e7946716777c3ea6e17b2a02fd0973cde2e8
Mihocsa Szilard <mihocsa48@gmail.com>
2021-06-03 21:13:14 +0200

    wrapping pokemonList in an IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,51 @@
-//creating the Pokemon database
-let pokemonList = [{
-    name: 'Pikachu',
-    height: 0.4,
-    type: 'electric'
-  },
-  {
-    name: 'Charizard',
-    height: 1.7,
-    type: ['fire', 'flying']
-  },
-  {
-    name: 'Onix',
-    height: 8.8,
-    type: ['rock', 'ground']
-  },
-  {
-    name: 'Cubone',
-    height: 0.4,
-    type: 'ground'
-  }
-];
+//wrapping pokemonList array in an IIFE
+let pokemonRepository = (function() {
+  //creating the Pokemon database
+  let pokemonList = [{
+      name: 'Pikachu',
+      height: 0.4,
+      type: 'electric'
+    },
+    {
+      name: 'Charizard',
+      height: 1.7,
+      type: ['fire', 'flying']
+    },
+    {
+      name: 'Onix',
+      height: 8.8,
+      type: ['rock', 'ground']
+    },
+    {
+      name: 'Cubone',
+      height: 0.4,
+      type: 'ground'
+    }
+  ];
 
-//console.log(pokemonList);
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+
+pokemonRepository.add({
+  name: 'Balbasaur',
+  height: 5.7,
+  type: ['grass', 'poison']
+});
+console.log(pokemonRepository.getAll());
 
 //listing all the pokemons and their hight
-pokemonList.forEach(function(item) {
+pokemonRepository.getAll().forEach(function(item) {
+  //if the pokemon is higher then 5 than write: Wow, this is big
   if (item.height > 5) {
     document.write(item.name + ' is ' + item.height + 'm high!' + ' - Wow! This is big!' + '<br>');
   } else {
