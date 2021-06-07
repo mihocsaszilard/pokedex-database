382ee352a99bfc4477a29d6982ea34ed09b2c28b
Mihocsa Szilard <mihocsa48@gmail.com>
2021-06-07 15:42:12 +0200

    listing pokemons with DOM interaction

--- js/scripts.js
+++ js/scripts.js
@@ -24,13 +24,9 @@ const pokemonRepository = (function() {
   ];
 
   function add(pokemon) {
-    pokemonList.push(pokemon);
-   }
-
-  function addv(pokemon){
-    if(typeof pokemon === 'object' && pokemon !== null){
+    if (typeof pokemon === 'object' && pokemon !== null) {
       pokemonList.push(pokemon);
-    }else{
+    } else {
       document.write('The given value is not an object or is null!' + '<br>')
     }
   }
@@ -39,33 +35,36 @@ const pokemonRepository = (function() {
     return pokemonList;
   }
 
-  const keysNumber = Object.keys(pokemonList).length; //logs the number of elements which is 4
-  console.log(keysNumber);
+  function addListItem(pokemon) {
+    let pokemonUl = document.querySelector('.pokemon-list');
+    let listItem = document.createElement('li');
+    let button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add('pokemon-button');
+    listItem.appendChild(button);
+    pokemonUl.appendChild(listItem);
+  }
+
+  //logs the number of elements which is 4
+  console.log(pokemonList.length);
 
   return {
     add: add,
-    addv:addv,
-    getAll: getAll
+    getAll: getAll,
+    addListItem: addListItem
   };
 })();
 
-pokemonRepository.addv();
-
-pokemonRepository.addv({
+pokemonRepository.add({
   name: 'Balbasaur',
   height: 5.7,
   type: ['grass', 'poison']
 });
 console.log(pokemonRepository.getAll());
 
-console.log(Object.keys(pokemonRepository.getAll()).length);  //length is 5
+console.log(pokemonRepository.getAll().length); //length is 5
 
 //listing all the pokemons and their hight
 pokemonRepository.getAll().forEach(function(item) {
-  //if the pokemon is higher then 5 than write: Wow, this is big
-  if (item.height > 5) {
-    document.write(item.name + ' is ' + item.height + 'm high!' + ' - Wow! This is big!' + '<br>');
-  } else {
-    document.write(item.name + ' is ' + item.height + 'm high!' + '<br>');
-  }
+  pokemonRepository.addListItem(item);
 })
